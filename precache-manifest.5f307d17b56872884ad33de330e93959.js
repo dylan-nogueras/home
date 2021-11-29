15179738cbf9d96e70c2f02e492378e631c148da
aure-nogueras <anogueras@correo.ugr.es>
2021-11-29 14:04:12 +0100

    Updates

---
+++ precache-manifest.5f307d17b56872884ad33de330e93959.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6888b77068471811b7af4998138be469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86447fb326446f3ffa92",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "38c0942bfbd86a1e9981",
+    "url": "/static/js/2.3f0aa88c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3f0aa88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86447fb326446f3ffa92",
+    "url": "/static/js/main.fcf3fbcd.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "91ec5693431d74514c65dc42eb07737d",
+    "url": "/static/media/albaicin.91ec5693.jpg"
+  },
+  {
+    "revision": "32e1e99f4000b4a1f6333585ba3eadd2",
+    "url": "/static/media/alhambra.32e1e99f.jpg"
+  },
+  {
+    "revision": "6507b3895e9a1991fe6f565e12290423",
+    "url": "/static/media/landscape.6507b389.jpg"
+  },
+  {
+    "revision": "cf301f2d1f5596ed615de971922cac54",
+    "url": "/static/media/perfil.cf301f2d.jpg"
+  },
+  {
+    "revision": "02574e02c976dc38e382b0d010266aee",
+    "url": "/static/media/resume.02574e02.pdf"
+  }
+]);
